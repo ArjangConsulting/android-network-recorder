f2db7681d1a42380e5a1cab8d43f8eba12d93c72
Mani Ramezan <mani.ramezan@gmail.com>
2026-07-26 14:09:15 -0400

    build: configure Android release publishing (#3)
    * build: configure Maven Central publishing
    
    * build: retain Release Please automation
    
    * ci: add PR checks and release token

--- api-trace-core/build.gradle.kts
+++ api-trace-core/build.gradle.kts
@@ -1,7 +1,31 @@
+import com.vanniktech.maven.publish.AndroidSingleVariantLibrary
+import org.gradle.api.publish.maven.MavenPublication
+import org.gradle.jvm.tasks.Jar
+
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
-    `maven-publish`
+    id("com.vanniktech.maven.publish")
+}
+
+mavenPublishing {
+    configure(
+        AndroidSingleVariantLibrary(
+            variant = "release",
+            sourcesJar = true,
+            publishJavadocJar = false,
+        )
+    )
+}
+
+val emptyJavadocJar = tasks.register<Jar>("emptyJavadocJar") {
+    archiveClassifier.set("javadoc")
+}
+
+afterEvaluate {
+    publishing.publications.withType<MavenPublication>().configureEach {
+        artifact(emptyJavadocJar)
+    }
 }
 
 android {
@@ -27,14 +51,4 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     testImplementation("junit:junit:4.13.2")
     testImplementation("org.json:json:20231013")
-}
-
-afterEvaluate {
-    publishing {
-        publications {
-            create<MavenPublication>("release") {
-                from(components["release"])
-            }
-        }
-    }
 }
